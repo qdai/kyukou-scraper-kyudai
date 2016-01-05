c8e32b38d588823caccc7ecaf5630c52daac3a3c
egy186 <egy186@yahoo.co.jp>
2016-01-06 01:47:10 +0900

    update test

--- test/fixtures/scraps/economics.js
+++ test/fixtures/scraps/economics.js
@@ -3,22 +3,22 @@
 module.exports = [
   new Error('Invalid eventDate on 【休講】10月5日(火) 1時限 学府 「subject3」 (teacher3教員)'),
   {
-    raw: '【休講】10月1日(木) 18:30~20:00 学部 「subject2」 (teacher2教員)',
+    raw: '【休講】10月6日(木) 18:30~20:00 学部 「subject2」 (teacher2教員)',
     about: '休講',
     link: 'http://www.econ.kyushu-u.ac.jp/student/kyuukou.php',
-    eventDate: new Date('2015-10-01T00:00+09:00'),
-    pubDate: new Date('2015-08-12T00:00+09:00'),
+    eventDate: new Date('2016-10-06T00:00+09:00'),
+    pubDate: new Date('2016-08-12T00:00+09:00'),
     period: '18:30~20:00',
     department: '経済学部',
     subject: 'subject2',
     teacher: 'teacher2'
   },
   {
-    raw: '【休講】10月5日(月) 4,5時限 学部 「subject1」 (teacher1教員)教室:room1',
+    raw: '【休講】10月3日(月) 4,5時限 学部 「subject1」 (teacher1教員)教室:room1',
     about: '休講',
     link: 'http://www.econ.kyushu-u.ac.jp/student/kyuukou_read.php?kind=&S_Category=C&S_View=&word=&page=1&B_Code=0001',
-    eventDate: new Date('2015-10-05T00:00+09:00'),
-    pubDate: new Date('2015-08-11T00:00+09:00'),
+    eventDate: new Date('2016-10-03T00:00+09:00'),
+    pubDate: new Date('2016-08-11T00:00+09:00'),
     period: '4,5',
     department: '経済学部',
     subject: 'subject1',
--- test/fixtures/scraps/education.js
+++ test/fixtures/scraps/education.js
@@ -3,11 +3,11 @@
 module.exports = [
   new Error('Invalid eventDate on 【補講】7月10日(木)4限 「subject4」(teacher4教員)'),
   {
-    raw: '【補講】6月15日(月)2、3限 「subject3」(teacher3教員) 教室:room3',
+    raw: '【補講】6月13日(月)2、3限 「subject3」(teacher3教員) 教室:room3',
     about: '補講',
     link: 'http://www.education.kyushu-u.ac.jp/topics/student_index',
-    eventDate: new Date('2015-06-15T00:00+09:00'),
-    pubDate: new Date('2015-05-25T00:00+09:00'),
+    eventDate: new Date('2016-06-13T00:00+09:00'),
+    pubDate: new Date('2016-05-25T00:00+09:00'),
     period: '2、3',
     department: '教育学部',
     subject: 'subject3',
@@ -15,11 +15,11 @@ module.exports = [
     room: 'room3'
   },
   {
-    raw: '【休講】6月2日(火)1限 campus2地区開講「subject2」(teacher2教員)',
+    raw: '【休講】6月7日(火)1限 campus2地区開講「subject2」(teacher2教員)',
     about: '休講',
     link: 'http://www.education.kyushu-u.ac.jp/topics/student_view/2',
-    eventDate: new Date('2015-06-02T00:00+09:00'),
-    pubDate: new Date('2015-05-15T00:00+09:00'),
+    eventDate: new Date('2016-06-07T00:00+09:00'),
+    pubDate: new Date('2016-05-15T00:00+09:00'),
     period: '1',
     department: '教育学部',
     subject: 'subject2',
