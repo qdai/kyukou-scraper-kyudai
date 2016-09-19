69e21ff985a4dfd390137e28bc0356ca3fb86422
egy186 <egy186@yahoo.co.jp>
2016-09-19 13:27:22 +0900

    update eslint to v3.5.0

--- test/fixtures/scraps/literature.js
+++ test/fixtures/scraps/literature.js
@@ -4,15 +4,15 @@ module.exports = [
   new Error('Invalid eventDate'),
   new TypeError('Cannot read property \'1\' of null on           2015年10月1日（木）          休講          後期・通常              木曜invalid period              subject1          teacher1          note1          2015年9月8日(1時24分)        '),
   {
-    raw: '\n          2015年10月1日（木）\n          休講\n          後期・通常\n              木曜2限\n              subject1\n          teacher1\n          note1\n          2015年9月8日(1時24分)\n        ',
     about: '休講',
-    link: 'http://www2.lit.kyushu-u.ac.jp/~syllabus/cgi-bin/class-schedule.cgi',
-    eventDate: new Date('2015-10-01T00:00+09:00'),
-    pubDate: new Date('2015-09-08T01:24+09:00'),
-    period: '2',
     department: '文学部',
+    eventDate: new Date('2015-10-01T00:00+09:00'),
+    link: 'http://www2.lit.kyushu-u.ac.jp/~syllabus/cgi-bin/class-schedule.cgi',
+    note: 'note1',
+    period: '2',
+    pubDate: new Date('2015-09-08T01:24+09:00'),
+    raw: '\n          2015年10月1日（木）\n          休講\n          後期・通常\n              木曜2限\n              subject1\n          teacher1\n          note1\n          2015年9月8日(1時24分)\n        ',
     subject: 'subject1',
-    teacher: 'teacher1',
-    note: 'note1'
+    teacher: 'teacher1'
   }
 ];
--- test/fixtures/scraps/science.js
+++ test/fixtures/scraps/science.js
@@ -2,26 +2,26 @@
 
 module.exports = [
   {
-    raw: '[[ 休講 ]]\n                     10月 7日 (金)\n                     2時限\n                    \n                    学科:化学\n                     学年:2学年\n                    科目:subject1\n                     (担当:teacher1)',
     about: '休講',
-    link: 'http://www.sci.kyushu-u.ac.jp/index.php?type=0&sel1=11&sel2=0',
-    eventDate: new Date('2016-10-07T00:00+09:00'),
-    period: '2',
     department: '理学部化学',
+    eventDate: new Date('2016-10-07T00:00+09:00'),
+    link: 'http://www.sci.kyushu-u.ac.jp/index.php?type=0&sel1=11&sel2=0',
+    period: '2',
+    raw: '[[ 休講 ]]\n                     10月 7日 (金)\n                     2時限\n                    \n                    学科:化学\n                     学年:2学年\n                    科目:subject1\n                     (担当:teacher1)',
     subject: 'subject1',
     teacher: 'teacher1'
   },
   {
-    raw: '[[ 連絡 ]]\n                     10月 6日 (木)\n                     4時限\n                    \n                    学科:数学\n                     学年:2学年\n                    科目:subject2\n                     (担当:teacher2)\n                    連絡事項:note2\n                    教室:room2',
     about: '連絡',
-    link: 'http://www.sci.kyushu-u.ac.jp/index.php?type=0&sel1=11&sel2=0',
-    eventDate: new Date('2016-10-06T00:00+09:00'),
-    period: '4',
     department: '理学部数学',
-    subject: 'subject2',
-    teacher: 'teacher2',
+    eventDate: new Date('2016-10-06T00:00+09:00'),
+    link: 'http://www.sci.kyushu-u.ac.jp/index.php?type=0&sel1=11&sel2=0',
+    note: 'note2',
+    period: '4',
+    raw: '[[ 連絡 ]]\n                     10月 6日 (木)\n                     4時限\n                    \n                    学科:数学\n                     学年:2学年\n                    科目:subject2\n                     (担当:teacher2)\n                    連絡事項:note2\n                    教室:room2',
     room: 'room2',
-    note: 'note2'
+    subject: 'subject2',
+    teacher: 'teacher2'
   },
   new Error('Invalid eventDate on [[ 休講 ]]                     10月 6日 (水)                     4時限                                        学科:数学                     学年:4学年                    科目:subject3                     (担当:subject3)')
 ];
