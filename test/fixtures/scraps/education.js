73773b01afbea0ca6ea72529fb71fbcc37936698
egy186 <egy186@yahoo.co.jp>
2017-01-09 21:14:09 +0900

    update test fixtures (2017)

--- test/fixtures/scraps/education.js
+++ test/fixtures/scraps/education.js
@@ -5,11 +5,11 @@ module.exports = [
   {
     about: '補講',
     department: '教育学部',
-    eventDate: new Date('2016-06-13T00:00+09:00'),
+    eventDate: new Date('2017-06-13T00:00+09:00'),
     link: 'http://www.education.kyushu-u.ac.jp/topics/student_index',
     period: '2、3',
-    pubDate: new Date('2016-05-25T00:00+09:00'),
-    raw: '【補講】6月13日(月)2、3限 「subject3」(teacher3教員) 教室:room3',
+    pubDate: new Date('2017-05-25T00:00+09:00'),
+    raw: '【補講】6月13日(火)2、3限 「subject3」(teacher3教員) 教室:room3',
     room: 'room3',
     subject: 'subject3',
     teacher: 'teacher3'
@@ -18,11 +18,11 @@ module.exports = [
     about: '休講',
     campus: 'campus2地区',
     department: '教育学部',
-    eventDate: new Date('2016-06-07T00:00+09:00'),
+    eventDate: new Date('2017-06-07T00:00+09:00'),
     link: 'http://www.education.kyushu-u.ac.jp/topics/student_view/2',
     period: '1',
-    pubDate: new Date('2016-05-15T00:00+09:00'),
-    raw: '【休講】6月7日(火)1限 campus2地区開講「subject2」(teacher2教員)',
+    pubDate: new Date('2017-05-15T00:00+09:00'),
+    raw: '【休講】6月7日(水)1限 campus2地区開講「subject2」(teacher2教員)',
     subject: 'subject2',
     teacher: 'teacher2'
   }
--- test/fixtures/scraps/science.js
+++ test/fixtures/scraps/science.js
@@ -4,21 +4,21 @@ module.exports = [
   {
     about: '休講',
     department: '理学部化学',
-    eventDate: new Date('2016-10-07T00:00+09:00'),
+    eventDate: new Date('2017-10-07T00:00+09:00'),
     link: 'http://www.sci.kyushu-u.ac.jp/index.php?type=0&sel1=11&sel2=0',
     period: '2',
-    raw: '[[ 休講 ]]\n                     10月 7日 (金)\n                     2時限\n                    \n                    学科:化学\n                     学年:2学年\n                    科目:subject1\n                     (担当:teacher1)',
+    raw: '[[ 休講 ]]\n                     10月 7日 (土)\n                     2時限\n                    \n                    学科:化学\n                     学年:2学年\n                    科目:subject1\n                     (担当:teacher1)',
     subject: 'subject1',
     teacher: 'teacher1'
   },
   {
     about: '連絡',
     department: '理学部数学',
-    eventDate: new Date('2016-10-06T00:00+09:00'),
+    eventDate: new Date('2017-10-06T00:00+09:00'),
     link: 'http://www.sci.kyushu-u.ac.jp/index.php?type=0&sel1=11&sel2=0',
     note: 'note2',
     period: '4',
-    raw: '[[ 連絡 ]]\n                     10月 6日 (木)\n                     4時限\n                    \n                    学科:数学\n                     学年:2学年\n                    科目:subject2\n                     (担当:teacher2)\n                    連絡事項:note2\n                    教室:room2',
+    raw: '[[ 連絡 ]]\n                     10月 6日 (金)\n                     4時限\n                    \n                    学科:数学\n                     学年:2学年\n                    科目:subject2\n                     (担当:teacher2)\n                    連絡事項:note2\n                    教室:room2',
     room: 'room2',
     subject: 'subject2',
     teacher: 'teacher2'
